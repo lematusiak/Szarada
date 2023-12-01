f30863410a01030c0be5d9913ee3d6467a672354
lematusiak <leematusiak@gmail.com>
2023-12-01 19:26:24 +0100

    Lekcja 055 (Łączenie Tekstu, Formatowanie WriteLine)

--- Szarada/Szarada/Program.cs
+++ Szarada/Szarada/Program.cs
@@ -1,98 +1,109 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-// Lekcja 054 Internet 05 KONWERSJA TYPÓW STRING > INT, INT > STRING
+// Lekcja 055 ŁĄCZENIE TEKSTU & FORMATOWANIE "WRITELINE"
 
-// Konwersja typów oznacza zmianę wartości z jednego typu danych na inny.
-// Na przykład, jeśli chcesz zmienić tekst na liczbę lub odwrotnie,
-// musisz skorzystać z konwersji typów.
-// Można to porównać do sytuacji w życiu codziennym,
-// kiedy musisz przekształcić coś w coś innego.
-// Na przykład, kiedy przeliczasz kilogramy na funty lub mile na kilometry.
-// Ważne jest, aby pamiętać, że niektóre konwersje mogą powodować utratę danych,
-// na przykład gdy przekształcasz liczbę zmiennoprzecinkową na liczbę całkowitą.
+// ŁĄCZENIE TEKSTU
+/*
+string s1 = " Witaj";
+string s2 = " Świecie";
+string s3 = s1 + s2;
+
+Console.WriteLine(  s3);
+Console.WriteLine(  s1 +  s2);    // Można również tak. 
+
+// Poniżej wyświetli się Witaj Świecie. Uwaga, jeśli nie zrobimy pauzy
+// w drugim stringu WitajŚwiecie wyświetli się w jednym ciągu.
+
+// Powyższe łączenie teksty to Kontagenacja. 
+// Nie jest to dodawanie, tylko doklejanie tekstu do tekstu
+// Odnosi się do łączenia ciągów znaków (stringów).
+// Jest przydatna, gdy chcemy stworzyć np. komunikat powitalny,
+// w którym wyświetlamy imię użytkownika wraz z przywitaniami.
+
+// FORMATOWANIE WRITELINE
+
+// W formatowaniu tekstu chodzi o to, że możemy łączyć dwie zmienne (s1 + s2)
+// oraz doklejać (wpisywać) tekst, jak poniżej
+// Ważne aby tekst wpisywać w "cudzysłowie"
+
+// Console.WriteLine( s1 +  s3  +  " Marniutki Globie !!! ");
+
+// Możemy w nawiasach klamrowych podać indeks, robimy to po przecinku 
+// Indeksy liczone są od zera, bo programiści uznali, 
+// że zero jest liczbą dodatnią
+// W poniższym indeks {0} wyświetli zmienną s1
+// Czyli "Witaj Świecie Marniutki Globie!!! Witaj Świecie
+
+// Console.WriteLine(  s1 + s2 + "Marniutki Globie!!! {0} {1}", s1, s2);
+
+// Więc widzimy do indeksu 0 jest przypisana zmienna s1 "Witaj" 
+// a do indeksu s2 "Świecie"
+
+// Mamy jeszcze swojego rodzaju mosyfikator slesz z literą \n
+// Tworzy nową linię
+// W tym przypadku indeks 1 w postaci zmiennej s2 będzie napisany linię niżej
+// Jeśli dodamy 3x \n \n \n jak poniżej zmienna s2 wyświetli sie trzy linie niżej
+// NIe musimy tworzyć dodatkowego cw, żeby była dodatkowa linia
+
+// Console.WriteLine(s1 + s2 + "Marniutki Globie!!! {0} \n\n\n {1}", s1, s2);
+*/
+
+
+// MODYFIKACJA WYGLĄDU KONSOLI
+
+// Modyfikacja może dotyczyć kolorów konsoli
+// Jeśli po jednym, a przed drugim cw wpiszemy Consol. (consol kropka)
+// to jest mnóstwo opcji, żeby skorzystać ze narzędzi visual studio
+// Np wpisując consol.col - wyświetli się BackgraundColor (kolor tła)
+// Foregraund Color (zmiana koloru) lub ResetColor (wyresetowanie kolorów)
+
+
 // 
-// STRING jest typem TEKSTOWYM, int jest typem LICZBOWYM
+
+string s1 = " Witaj";
+string s2 = " Świecie";
+string s3 = s1 + s2;
+
+Console.WriteLine(s3);
+Console.WriteLine(s1 + s2);
+Console.ForegroundColor = ConsoleColor.Red ;
+Console.WriteLine(s1 + s2 + "Marniutki Globie!!! {0} \n\n\n {1}", s1, s2);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(s1 + s2 + "Marniutki Globie!!! {0} \n\n\n {1}", s1, s2);
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine(s1 + s2 + "Marniutki Globie!!! {0} \n\n\n {1}", s1, s2);
+Console.ResetColor();
+Console.WriteLine(s1 + s2 + "Marniutki Globie!!! {0} \n\n\n {1}", s1, s2);
+Console.ReadKey();       // tutaj zatrzymujemy program - od tego jest to polecenie
+Console.Clear();         // po kliknięciu obojętnie jakiego klawisza, wszystko z konsoli zniknie       
 
 
-// gdybyśmy chcieli przekonwertować niejawnie int na string int i  = s + 2, 
-// to nie ma takiej możliwości. trzeba użyć innego sposobu
+//Poniżej konsole z akapitów 68,69 wyświetlą się normalnie\
+// z akapitu 71 na czerwono
+// z akapitu 73 na niebiesko
+//
+// kolory są tutaj tylko polami dlatego nie są  przechowywane w nawiasach
+// (zapis = ConsoleColor.Blue), są to tylko liczby (oznaczające dany kolor)
+// Nie są to metod, ponieważ nie robią żadnych obliczeń, tak jak 
+// powiedzieliśmy są tylko przypisanymi liczbami np. 12 dla koloru red
+// Używając Console BackgroundColor zmieniamy tło tekstu (akapit 75 - green)
 
-// a więc musimy przekonwertować typ tekstowy na typ liczbowy
+// Consol.ResetColour (); to metoda ponieważ ona coś restartuje,
+// nie przechowuje informacji pojedyńczej tylko wykonuje jakieś działania
+// Widzimy, że jest to metoda, są nawiasy, i tak będziemy to rozróżniać
+// Akapit  77 (przedostatni) ustawi nam wszystko do ustawień domyślych,
+// czyli ustawi czarne tło, biały tekst
 
-class Program
-{
-static void Main(string[]args)
-    {
+// Akapit 78 (zatrzymujemy program przez Console.ReadKey();
+// i chcemy wszystko wyczyścić przez Console.Clear(); akapit 79
+// UWAGA: po wciśnięciu obojętnie jakiego klawisza, wszystko z konsoli
+// zostanie usunięte - zadziała clear - czyszczenie!!!
 
-        // KONWERSJA STRING NA INT
-
-        string s = Console.ReadLine(); // string s jest tylko tekstem
-                                       // nie jest wartością logiczną
-        
-        int i = int.Parse(s) + 2; // przez Parse wyciągamy wartość liczbową
-                                  // jaka siedzi w typie string s
-                                  // od teraz jeśli coś będziemy na konsoli liczyli
-                                  // będzie dodawało, przestanie być tekstem
-
-        Console.WriteLine( i); // Jeśli wpiszemy 5, wyświetli się 7
-
-       // 7 dlatego, że  - int.Parse(s) - będzie konkretnie tą naszą wpisaną 5
-       // 5 do której dodamy 2 i wyjdzie to co równa się int i. (int i)
-       // A WIĘC STRING S stał się WARTOŚCIĄ LOGICZNĄ w postaci INT.PARSE (S)
-       // W taki sposób zamieniamy wartość tekstową na logiczną
-       // Jeśli za int.Parse(s) wpiszemy 9, wyświetli się 11 itd, itd
+// Dość Pan! Dziekuję 30 listopada 2023
 
 
-// KONWERSJA W DRUGĄ STRONĘ (Z INT NA STRING)
-
-//  s = i; (jeśli tak chcielibyśmy przekonwertować wyświetli się błąd,
-// nie można niejawnie tak przekonwertować int na string
-// czyli pamiętamy wlać butlę z wodą z inta do butli stringa
-
-/* ZMIENNA i JEST JUŻ OBIEKTEM i przechowuje w sobie dziedziczone różne metody
- * do których po kropce możemy uzyskać dostęp
- * 
- * Dlatego nie da się jej przekonwertować, ale możemy coś zaradzić,
- * czyli po kropce możemy uzyskać dostęp do jednej z metod.
- * Jedną z metod jest właśnie ToString. - a tu string oznacza 
- * zamień na typ tekstowy. (Czyli będzie s = i.ToString();) 24.40
- * 
- * Zmienna staje się obiektem, gdy przypisujemy do niej instancję 
- * (konkretny egzemplarz) klasy.  
- * Innymi słowy, kiedy używamy konstruktora klasy, tworząc nowy obiekt, 
- * i przypisujemy go do zmiennej. 
- * Na przykład, jeśli mamy klasę o nazwie "Samochod" 
- * i tworzymy nowy obiekt tej klasy, np. samochod1, używając konstruktora,
- * to możemy przypisać ten obiekt do zmiennej samochod1. 
- * Od tego momentu, zmienna samochod1 jest obiektem typu "Samochod" 
- * i możemy używać go do wywoływania metod 
- * i dostępu do właściwości zdefiniowanych w klasie "Samochod".
- * Podobnie jak w życiu codziennym, kiedy otrzymujemy np. klucz do mieszkania, 
- * staje się on przedmiotem, który możemy używać do otwierania drzwi.
- * 
- * 
- * 
- * 
- * 
- */
-
-// SPOSÓB NA KOWNWERSJĘ INT NA STRING
-
-
-s = i.ToString();   // ToString oznacza: zamień na typ tekstowy
-Console.WriteLine(s);
 
 Console.ReadKey();
 
 
-
-
-}
-
-
-
-
-
-
-}
